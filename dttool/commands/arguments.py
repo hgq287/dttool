d9b67e0667e2cadf3ce465e888493d2ad440d921
Hg Q <hgq287@gmail.com>
2024-11-17 15:28:02 +0700

    * add parser for command-line options, arguments ...

--- dttool/commands/arguments.py
+++ dttool/commands/arguments.py
@@ -3,6 +3,16 @@ from functools import partial
 from pathlib import Path
 from typing import Any, Optional, Union
 
+from dttool.commands.cli_options import AVAILABLE_CLI_OPTIONS
+from dttool.constants import DEFAULT_CONFIG
+
+ARGS_COMMON = ["logfile", "version"]
+
+NO_CONF_REQURIED = [
+]
+
+NO_CONF_ALLOWED = []
+
 class Arguments:
   """
   Arguments Class. Manage the arguments received by the cli
@@ -11,3 +21,40 @@ class Arguments:
   def __init__(self, args: Optional[list[str]]) -> None:
     self.args = args
     self._parsed_arg: Optional[Namespace] = None
+
+  def get_parsed_arg(self) -> dict[str, Any]:
+    if self._parsed_arg is None:
+      self._build_subcommands()
+      self._parse_args = self._parse_args()
+
+    return vars(self._parsed_arg)
+
+  def _parse_args(self) -> Namespace:
+    """
+    Parses given arguments and returns an argparse Namespace instance.
+    """
+    parsed_arg = self.parser.parse_args(self.args)
+    return parsed_arg
+
+  def _build_args(
+      self, optionlist: list[str], parser: Union[ArgumentParser, _ArgumentGroup]
+  ) -> None:
+    for val in optionlist:
+      opt = AVAILABLE_CLI_OPTIONS[val]
+      parser.add_argument(*opt.cli, dest=val, **opt.kwargs)
+
+  def _build_subcommands(self) -> None:
+    """
+    Builds and attaches all subcommands.
+    :return: None
+    """
+    # Build shared arguments (as group Common Options)
+    _common_parser = ArgumentParser(add_help=False)
+    group = _common_parser.add_argument_group("Common arguments")
+    self._build_args(optionlist=ARGS_COMMON, parser=group)
+
+    # Build main command
+    self.parser = ArgumentParser(
+        prog="dttool", description="A useful tool for software developers"
+    )
+    self._build_args(optionlist=["version"], parser=self.parser)
--- dttool/main.py
+++ dttool/main.py
@@ -2,6 +2,9 @@ import logging
 import sys
 from typing import Any, Optional
 
+from dttool import __version__
+from dttool.exceptions import OperationalException
+
 # check min. python version (>= 3.10 required)
 if sys.version_info < (3, 10):  # pragma: no cover
   sys.exit("Dttool requires Python version >= 3.10")
@@ -9,12 +12,27 @@ if sys.version_info < (3, 10):  # pragma: no cover
 from dttool.commands import Arguments
 
 logger = logging.getLogger("dttool")
+
 def main(sysargv: Optional[list[str]] = None) -> None:
   return_code: Any = 1
   try:
     arguments = Arguments(sysargv)
     args = arguments.get_parsed_arg()
-    print(args)
+
+    # Call subcommand.
+    if "func" in args:
+        logger.info(f"freqtrade {__version__}")
+        return_code = args["func"](args)
+    else:
+        # No subcommand was issued.
+        raise OperationalException(
+            "Usage of Dttool requires a subcommand to be specified.\n"
+            "To have the bot executing trades in live/dry-run modes, "
+            "depending on the value of the `dry_run` setting in the config, run Dttool "
+            "as `dttool start [options...]`.\n"
+            "To see the full list of options available, please use "
+            "`dttool --help` or `dttool <command> --help`."
+        )
   finally:
     sys.exit(return_code)
   
