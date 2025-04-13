54d60bad4244373cdbbb28f28628d8ee81282fda
Hg Q <htq287@gmail.com>
2025-04-13 22:42:08 +0700

    feat: create API server, using uvicorn & FastAPI

--- dttool/rpc/api_server/webserver.py
+++ dttool/rpc/api_server/webserver.py
@@ -1,4 +1,24 @@
 import logging
+from typing import Any
+
+import orjson
+import uvicorn
+from fastapi import Depends, FastAPI
+
+from starlette.responses import JSONResponse 
+
+
+class FTJSONResponse(JSONResponse):
+  media_type = "application/json"
+
+  def render(self, content: Any) -> bytes:
+    """
+    Render the content to JSON format using orjson.
+
+    :param content: The content to be rendered.
+    :return: The rendered JSON bytes.
+    """
+    return orjson.dumps(content, option=orjson.OPT_SERIALIZE_NUMPY)
 
 class ApiServer:
   """
@@ -15,3 +35,35 @@ class ApiServer:
     self.host = host
     self.port = port
     print(f"ApiServer initialized at {self.host}:{self.port}")
+
+    self.app = FastAPI(
+      title="Dttool API",
+      description="API for dttool",
+      version="0.1.0",
+      default_response_class=FTJSONResponse,
+    )
+
+    self.start_api()
+
+  def start_api(self):
+    """
+    Start the API server.
+    """
+    print(f"Starting API server at {self.host}:{self.port}")
+    
+    uvconfig = uvicorn.Config(
+      self.app,
+      port=4096,
+      host='127.0.0.1',
+      use_colors=False,
+      log_config=None,
+    )
+    
+    try:
+      server = uvicorn.Server(uvconfig)
+      server.run()
+    except Exception as e:
+      logging.error(f"Error starting server: {e}")
+      raise
+    finally:
+      logging.info("Server stopped")
